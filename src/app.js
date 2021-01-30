a8a22f6d53b072a5af702d473dc214000a0ffa51
Charmant101 <daylayomoarukhe@yahoo.com>
2021-01-30 13:46:21 +0100

    refactored and added handler for custom error responses

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const logger = require("morgan");
 const validationRoutes = require("./routes/validate");
 dotenv.config();
 const port = process.env.PORT || 5000;
-
+const errorHandler = require("./middleware/error");
 if (process.env.NODE_ENV === "development") {
   app.use(logger("dev"));
 }
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(validationRoutes);
 
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server is listening in ${process.env.NODE_ENV} on port ${port}`);
 });
--- src/controllers/validate.js
+++ src/controllers/validate.js
@@ -1,37 +1,27 @@
-let rulesValidator = require("./validator.js");
+const rulesValidator = require("./validator.js");
+const ErrorResponse = require("../utils/errorResponse");
 
-exports.createValidation = (req, res) => {
+exports.createValidation = (req, res, next) => {
   // check if the json payload is valid
   if (req.body.constructor !== Object || Object.keys(req.body).length === 0) {
-    return res.status(400).json({
-      message: "Invalid JSON payload passed.",
-      status: "error",
-      data: null,
-    });
+    return next(new ErrorResponse("Invalid JSON payload passed.", 400));
   }
 
   //   check if rule and data field is present
   const body = req.body;
   if (body.rule === undefined || body.data === undefined) {
     const message =
-      body.rule === undefined ? `rule is required.` : `data is required`;
-    return res.status(400).json({
-      message: message,
-      status: "error",
-      data: null,
-    });
+      body.rule === undefined ? `rule is required.` : `data is required.`;
+    return next(new ErrorResponse(message, 400));
   }
 
   let objectConstructor = Object;
   let arrayConstructor = Array;
   let stringConstructor = String;
 
+  // to check if the rule is a valid object
   if (body.rule.constructor !== objectConstructor) {
-    return res.status(400).json({
-      message: "rule should be an object.",
-      status: "error",
-      data: null,
-    });
+    return next(new ErrorResponse("rule should be an object.", 400));
   }
 
   //   check if the rule has to have a field,condition and condition_value
@@ -42,21 +32,16 @@ exports.createValidation = (req, res) => {
     missing_field = "condition_value";
 
   if (missing_field) {
-    return res.status(400).json({
-      message: `${missing_field} is required.`,
-      status: "error",
-      data: null,
-    });
+    return next(new ErrorResponse(`${missing_field} is required.`, 400));
   }
 
+  // to validate if our condition set meets the desired condition
   let condition = body.rule.condition;
   let valid_conditions = ["eq", "neq", "gt", "gte", "contains"];
   if (!valid_conditions.includes(condition)) {
-    return res.status(400).json({
-      message: `condition should be one of ${valid_conditions}`,
-      status: "error",
-      data: null,
-    });
+    return next(
+      new ErrorResponse(`condition should be one of ${valid_conditions}`, 400)
+    );
   }
 
   let dataConstructor = body.data.constructor;
@@ -65,11 +50,9 @@ exports.createValidation = (req, res) => {
     dataConstructor !== stringConstructor &&
     dataConstructor !== arrayConstructor
   ) {
-    return res.status(400).json({
-      message: "data should be an array, string or object.",
-      status: "error",
-      data: null,
-    });
+    return next(
+      new ErrorResponse("data should be an array, string or object.", 400)
+    );
   }
 
   let value =
@@ -78,7 +61,7 @@ exports.createValidation = (req, res) => {
       : body.rule.field.split(".");
   let data = body.data;
 
-  //we need to cgeck that value.length is at least one and return an approrpiate error
+  //we need to check that value.length is at least one and return an approrpiate error
   // if that is not the case
 
   if (data.constructor === arrayConstructor) {
@@ -86,41 +69,36 @@ exports.createValidation = (req, res) => {
     let index_out_bounds = data.length < value;
 
     if (invalid_field_type || index_out_bounds) {
-      return res.status(400).json({
-        message: `field ${body.rule.field} is missing from data`,
-        status: "error",
-        data: null,
-      });
+      return next(
+        new ErrorResponse(`field ${body.rule.field} is missing from data`, 400)
+      );
     }
   } else if (data.constructor === stringConstructor) {
     if (value.constructor === Number || value.length > 1) {
-      return res.status(400).json({
-        message: `field ${body.rule.field} is missing from data`,
-        status: "error",
-        data: null,
-      });
+      return next(
+        new ErrorResponse(`field ${body.rule.field} is missing from data`, 400)
+      );
     }
   } else if (data.constructor === objectConstructor) {
     //user.address split now has [user, address] 2 things
     // valid: data: {user: {name: dele, address: lagos}}
     //invalid data: {user: dele, phone: 1234}
     if (value.constructor !== arrayConstructor) {
-      return res.status(400).json({
-        message: `field ${body.rule.field} is missing from data.`,
-        status: "error",
-        data: null,
-      });
+      return next(
+        new ErrorResponse(`field ${body.rule.field} is missing from data`, 400)
+      );
     }
 
     let index = 0;
     while (index < value.length) {
       if (!data.hasOwnProperty(value[index])) {
-        //return thr error
-        return res.status(400).json({
-          message: `field ${body.rule.field} is missing from data.`,
-          status: "error",
-          data: null,
-        });
+        //return the error
+        return next(
+          new ErrorResponse(
+            `field ${body.rule.field} is missing from data`,
+            400
+          )
+        );
       }
       data = data[value[index]];
       index++;
@@ -131,7 +109,7 @@ exports.createValidation = (req, res) => {
   let result = rulesValidator.validator(body.rule, body.data);
   if (result.status) {
     res.status(200).json({
-      message: `field ${body.rule.field} succesfully validated.`,
+      message: `field ${body.rule.field} successfully validated.`,
       status: "success",
       data: {
         validation: {
@@ -148,11 +126,13 @@ exports.createValidation = (req, res) => {
       message: `field ${body.rule.field} failed validation.`,
       status: "error",
       data: {
-        error: true,
-        field: body.rule.field,
-        field_value: result.field_value,
-        condition: body.rule.condition,
-        condition_value: body.rule.condition_value,
+        validation: {
+          error: true,
+          field: body.rule.field,
+          field_value: result.field_value,
+          condition: body.rule.condition,
+          condition_value: body.rule.condition_value,
+        },
       },
     });
   }
